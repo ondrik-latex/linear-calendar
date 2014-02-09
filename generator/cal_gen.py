49eb986a43dba1c422d6ed5146ca2f98753d4581
Ondra Lengal <ondra.lengal@gmail.com>
2014-02-09 22:48:39 +0100

    generator: add basic generator

---
+++ generator/cal_gen.py
@@ -0,0 +1,19 @@
+from datetime import date
+import re
+
+LATEX_SOURCE = "../linear_calendar.tex"
+
+FIRST_DATE = date(2014, 1, 1)
+LAST_DATE = date(2014, 12, 14)
+
+if __name__ == "__main__":
+    begin_date = FIRST_DATE
+    end_date = LAST_DATE
+    with open(LATEX_SOURCE) as latex_file:
+        for line in latex_file:
+            if (re.search(r"^\\newcommand{\\firstdate}{[^}]*}$", line)):
+                line = re.sub(r"{[^{]*}$", "{" + begin_date.isoformat() + "}", line)
+            if (re.search(r"^\\newcommand{\\lastdate}{[^}]*}$", line)):
+                line = re.sub(r"{[^{]*}$", "{" + end_date.isoformat() + "}", line)
+            print(line, end = '')
+
