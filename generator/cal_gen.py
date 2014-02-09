148e4477faaf9ef4026941bc62718575c1b37862
Ondra Lengal <ondra.lengal@gmail.com>
2014-02-09 23:23:14 +0100

    generator: split into pages

--- generator/cal_gen.py
+++ generator/cal_gen.py
@@ -1,19 +1,31 @@
-from datetime import date
 import re
 
+from datetime import date, timedelta
+from subprocess import call
+
 LATEX_SOURCE = "./template.tex"
+OUTPUT_BASE = "./output"
 
 FIRST_DATE = date(2014, 1, 1)
-LAST_DATE = date(2014, 12, 14)
+LAST_DATE = date(2014, 12, 31)
+
+MAX_DAYS = 34
 
 if __name__ == "__main__":
     begin_date = FIRST_DATE
-    end_date = LAST_DATE
-    with open(LATEX_SOURCE) as latex_file:
-        for line in latex_file:
-            if (re.search(r"^\\newcommand{\\firstdate}{[^}]*}$", line)):
-                line = re.sub(r"{[^{]*}$", "{" + begin_date.isoformat() + "}", line)
-            if (re.search(r"^\\newcommand{\\lastdate}{[^}]*}$", line)):
-                line = re.sub(r"{[^{]*}$", "{" + end_date.isoformat() + "}", line)
-            print(line, end = '')
 
+    i = 0
+    while begin_date <= LAST_DATE:
+        end_date = min(begin_date + timedelta(days=MAX_DAYS), LAST_DATE)
+        file_name = OUTPUT_BASE + i.__str__() + ".tex"
+        with open(file_name, mode="w") as out_file:
+            with open(LATEX_SOURCE, mode="r") as latex_file:
+                for line in latex_file:
+                    if (re.search(r"^\\newcommand{\\firstdate}{[^}]*}$", line)):
+                        line = re.sub(r"{[^{]*}$", "{" + begin_date.isoformat() + "}", line)
+                    if (re.search(r"^\\newcommand{\\lastdate}{[^}]*}$", line)):
+                        line = re.sub(r"{[^{]*}$", "{" + end_date.isoformat() + "}", line)
+                    out_file.write(line)
+        call(["pdflatex", file_name])
+        begin_date = end_date + timedelta(days=1)
+        i = i+1
